988fdbd70fb86992f74718d401113c53c45e048f
Quangder <quincyquansyquang@gmail.com>
2016-10-12 21:37:23 -0700

    Event Class
    Added the event class skeleton

---
+++ app/src/main/java/com/example/davidlevitsky/friendsconnect/Event.java
@@ -0,0 +1,13 @@
+
+public class Event {
+	private String time;
+	private String location;
+	private String date;
+	
+	public Event(String time, String location, String date)
+	{
+		this.time = time;
+		this.location = location;
+		this.date = date;
+	}
+}
