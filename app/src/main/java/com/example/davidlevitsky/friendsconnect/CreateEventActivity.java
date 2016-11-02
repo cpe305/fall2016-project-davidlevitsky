c87375507895749795a28f14a066294852441cb0
davidlevitsky <dlevitsk@calpoly.edu>
2016-11-02 11:12:56 -0700

    Adding more functionality to CreateEventActivity

--- app/src/main/java/com/example/davidlevitsky/friendsconnect/CreateEventActivity.java
+++ app/src/main/java/com/example/davidlevitsky/friendsconnect/CreateEventActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -19,14 +21,18 @@ import java.util.ArrayList;
  * EventsList and append new events to it.
  */
 public class CreateEventActivity extends AppCompatActivity {
+    private CheckBox timeCheckbox;
+    private EditText fromTime;
+    private EditText toTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+        timeCheckbox = (CheckBox)findViewById(R.id.checkBox);
+        fromTime = (EditText)findViewById(R.id.etFromTime);
+        toTime = (EditText)findViewById(R.id.etToTime);
         setup();
-
-
     }
 
     public void setup() {
@@ -43,13 +49,34 @@ public class CreateEventActivity extends AppCompatActivity {
             }
 
         });
+        timeCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView,boolean isChecked) {
+                if (isChecked) {
+                    fromTime.setText("12:00");
+                    toTime.setText("12:00");
+                }
+                else {
+                    fromTime.setText("");
+                    toTime.setText("");
+                }
+            }
+        }
+        );
+        String currentDate = getIntent().getStringExtra("date");
+        EditText etDate = (EditText)findViewById(R.id.etDate);
+        etDate.setText(currentDate);
 
     }
 
     public void createNewEvent() {
         EditText etEventName = (EditText)findViewById(R.id.etEventName);
-        String temp = etEventName.toString();
-        Toast toast = Toast.makeText(getApplicationContext(), "creation works: " + temp, Toast.LENGTH_LONG);
+        String name = etEventName.getText().toString();
+        Toast toast = Toast.makeText(getApplicationContext(), "creation works: " + name, Toast.LENGTH_LONG);
+        Event newEvent = new Event(name, "time", "location", "date");
+        EventsList eventsList = EventsList.getInstance();
+        eventsList.addEvent(newEvent);
         toast.show();
     }
 
