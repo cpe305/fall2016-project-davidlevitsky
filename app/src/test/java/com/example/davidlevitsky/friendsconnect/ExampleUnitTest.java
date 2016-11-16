15c46b460d68d81538ec450099b04b2646fa19d7
davidlevitsky <dlevitsk@calpoly.edu>
2016-11-16 11:33:13 -0800

    Setting up SonarQube and Unit Tests

--- app/src/test/java/com/example/davidlevitsky/friendsconnect/ExampleUnitTest.java
+++ app/src/test/java/com/example/davidlevitsky/friendsconnect/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.example.davidlevitsky.friendsconnect;
 
+import android.app.usage.UsageEvents;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,8 +11,27 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
+    Contact contact;
+
+    @Before
+    public void makeUser() {
+        contact = new Contact("name", "e-mail");
+    }
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void contactConstructor() {
+        assertEquals(contact.getName(), "name");
+
+    }
+
+
+    public static void main(String [] args) {
+        org.junit.runner.JUnitCore.main("ExampleUnitTest");
     }
 }
