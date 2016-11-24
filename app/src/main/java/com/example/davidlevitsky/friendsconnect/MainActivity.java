627d12d515199493fb03a2c419ec9c658cd21268
davidlevitsky <dlevitsk@calpoly.edu>
2016-11-23 23:27:05 -0800

    Pulling E-mails from Contact List on Phone

--- app/src/main/java/com/example/davidlevitsky/friendsconnect/MainActivity.java
+++ app/src/main/java/com/example/davidlevitsky/friendsconnect/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.davidlevitsky.friendsconnect;
 
+import android.annotation.TargetApi;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.Image;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -15,6 +25,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -22,11 +33,14 @@ import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.RealmResults;
 
+import static android.Manifest.permission.READ_CONTACTS;
+
 public class MainActivity extends AppCompatActivity {
 
     private String currentDate;
     private Realm realm;
     private EventAdapter mEventAdapter;
+    private static final int REQUEST_READ_CONTACTS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         RealmSetup();
         setup();
+        ArrayList<String> temp = getNameEmailDetails();
+        for (String s : temp) {
+            Toast toast = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
+            toast.show();
+        }
+
 
     }
 
@@ -119,6 +139,47 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+    public ArrayList<String> getNameEmailDetails() {
+        ArrayList<String> emlRecs = new ArrayList<String>();
+        HashSet<String> emlRecsHS = new HashSet<String>();
+        Context context = getApplicationContext();
+        ContentResolver cr = context.getContentResolver();
+        String[] PROJECTION = new String[] { ContactsContract.RawContacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME,
+                ContactsContract.Contacts.PHOTO_ID,
+                ContactsContract.CommonDataKinds.Email.DATA,
+                ContactsContract.CommonDataKinds.Photo.CONTACT_ID };
+        String order = "CASE WHEN "
+                + ContactsContract.Contacts.DISPLAY_NAME
+                + " NOT LIKE '%@%' THEN 1 ELSE 2 END, "
+                + ContactsContract.Contacts.DISPLAY_NAME
+                + ", "
+                + ContactsContract.CommonDataKinds.Email.DATA
+                + " COLLATE NOCASE";
+        String filter = ContactsContract.CommonDataKinds.Email.DATA + " NOT LIKE ''";
+        Cursor cur = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, PROJECTION, filter, null, order);
+        if (cur.moveToFirst()) {
+            do {
+                // names comes in hand sometimes
+                String name = cur.getString(1);
+                String emlAddr = cur.getString(3);
+
+                // keep unique only
+                if (emlRecsHS.add(emlAddr.toLowerCase())) {
+                    emlRecs.add(emlAddr);
+                }
+            } while (cur.moveToNext());
+        }
+
+        cur.close();
+        Toast toast = Toast.makeText(getApplicationContext(), "success " + Integer.toString(emlRecs.size()), Toast.LENGTH_LONG);
+        toast.show();
+        return emlRecs;
+    }
+
 
 
 }
